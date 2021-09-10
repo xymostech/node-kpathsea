1b4d293ff6c67b0603f38ae7fc3b5af469172fd7
Jim Fowler <fowler@math.osu.edu>
2021-09-10 11:42:56 -0400

    possibly better shell.nix

--- shell.nix
+++ shell.nix
@@ -1,8 +1,9 @@
 { pkgs ? import <nixpkgs> {} }:
   pkgs.mkShell {
     # nativeBuildInputs is usually what you want -- tools you need to run
-    # nativeBuildInputs = [ pkgs.texlive.bin ];
+    nativeBuildInputs = [ pkgs.nodePackages.node-gyp ];
     CFLAGS_CC="-I${pkgs.texlive.bin.core}/include";
+    CXXFLAGS="-I${pkgs.texlive.bin.core}/include";
   }
  
 
